d1f7597873fd74d5784006789d7cf218efbb1823
martinosoriolopez <me@martinosorio.com>
2016-12-20 17:39:51 -0500

    Created menu.xml, and made the main activity inflate it. ICONS ARENT WORKING

--- app/src/main/java/com/example/martinosorio/mixedhomework/MainActivity.java
+++ app/src/main/java/com/example/martinosorio/mixedhomework/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.martinosorio.mixedhomework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        //super.onCreateOptionsMenu(menu);
+        //menu.add("Home").setIcon(R.drawable.one);
+        return true;
+    }
+
+    public void onDialogClick(View view) {
+    }
+
+    public void onFragmentClick(View view) {
+    }
+
+    public void onPrefsClick(View view) {
     }
 }
